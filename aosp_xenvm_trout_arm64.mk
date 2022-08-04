6620df12e3f4b47a7bc4d18d8abc4353de8de88c
Vladyslav Goncharuk <vladyslav_goncharuk@epam.com>
2022-08-04 13:37:54 +0300

    Override the 'init.vendor.rc' file
    We need to prevent renaming of the eth0 network interface on start-up
    of the system. This patch introduced init.vendor.rc file, which
    comments out the following line:
    
    start rename_eth0
    
    - Introduced 'shared/config/init.vendor.rc', which overrides the
    'device/google/cuttlefish/shared/config/init.vendor.rc' file from the
    cuttlefish device
    - Introduced 'aosp_xenvm_trout_common.mk', which copies
    'device/epam/aosp-xenvm-trout/shared/config/init.vendor.rc' as
    '/vendor/etc/init/init.cutf_cvm.rc' on the target
    - Included 'device/epam/aosp-xenvm-trout/aosp_xenvm_trout_common.mk'
    into the 'aosp_xenvm_trout_arm64.mk'
    - Included 'device/epam/aosp-xenvm-trout/aosp_xenvm_trout_common.mk'
    into the 'aosp_xenvm_trout_x86.mk'
    
    Signed-off-by: Vladyslav Goncharuk <vladyslav_goncharuk@epam.com>
    Reviewed-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>

--- aosp_xenvm_trout_arm64.mk
+++ aosp_xenvm_trout_arm64.mk
@@ -1,5 +1,7 @@
 $(call inherit-product, device/google/trout/aosp_trout_arm64.mk)
 
+include device/epam/aosp-xenvm-trout/aosp_xenvm_trout_common.mk
+
 PRODUCT_NAME := aosp_xenvm_trout_arm64
 PRODUCT_DEVICE := xenvm_trout_arm64
 PRODUCT_MODEL := xenvm arm64 trout
--- aosp_xenvm_trout_x86.mk
+++ aosp_xenvm_trout_x86.mk
@@ -1,5 +1,7 @@
 $(call inherit-product, device/google/trout/aosp_trout_x86.mk)
 
+include device/epam/aosp-xenvm-trout/aosp_xenvm_trout_common.mk
+
 PRODUCT_NAME := aosp_xenvm_trout_x86
 PRODUCT_DEVICE := xenvm_trout_x86
 PRODUCT_MODEL := xenvm x86 trout
