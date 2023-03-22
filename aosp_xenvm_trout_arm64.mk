8c45315f674844129e1276a0959f3520694dd956
Dmytro Terletskyi <dmytro_terletskyi@epam.com>
2023-03-22 13:00:34 +0200

    Allow usage of the external prebuilt kernel
    - Added functionality, which allows usage of the external prebuilt
    kernel and modules in the 'xenvm_trout_arm64' device
    
    Signed-off-by: Dmytro Terletskyi <dmytro_terletskyi@epam.com>
    Reviewed-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>

--- aosp_xenvm_trout_arm64.mk
+++ aosp_xenvm_trout_arm64.mk
@@ -1,3 +1,43 @@
+# TARGET_PREBUILT_KERNEL - environment variable which could contain path to the prebuilt kernel.
+ifneq ($(TARGET_PREBUILT_KERNEL),)
+   # Set TARGET_KERNEL_PATH variable which defines which kernel will be used in trout device.
+   TARGET_KERNEL_PATH := $(TARGET_PREBUILT_KERNEL)
+
+   # Flag to supress errors in case of usage of the new kernel
+   # which is not mentioned in original configs and compatibility matrices.
+   PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
+   TARGET_KERNEL_USE := 6.1
+endif
+
+# TARGET_PREBUILT_MODULES_DIR - environment variable which could contain path to the prebuilt kernel modules.
+ifneq ($(TARGET_PREBUILT_MODULES_DIR),)
+   # Set BOARD_VENDOR_RAMDISK_KERNEL_MODULES variable which defines which kernel modules will be used in trout device.
+   BOARD_VENDOR_RAMDISK_KERNEL_MODULES := $(filter-out $(TARGET_PREBUILT_MODULES_DIR),$(shell find $(TARGET_PREBUILT_MODULES_DIR) -type f -name *.ko))
+
+   # Flag to disable stripping kernel modules during AOSP build.
+   BOARD_DO_NOT_STRIP_VENDOR_RAMDISK_MODULES := true
+endif
+
+# BOARD_KERNEL_CMDLINE += \
+#    androidboot.qemu=1 \
+#    androidboot.selinux=permissive \
+#    androidboot.fstab_suffix=trout \
+#    androidboot.hardware=cutf_cvm \
+#    androidboot.slot_suffix=_a \
+#    androidboot.hardware.gralloc=minigbm \
+#    androidboot.hardware.hwcomposer=drm_minigbm \
+#    androidboot.hardware.egl=mesa \
+#    androidboot.logcat=*:V \
+#    androidboot.vendor.vehiclehal.server.cid=2 \
+#    androidboot.vendor.vehiclehal.server.port=9300 \
+#    androidboot.vendor.vehiclehal.server.psf=/data/data/power.file \
+#    androidboot.vendor.vehiclehal.server.pss=/data/data/power.socket \
+#    androidboot.boot_devices=4010000000.pcie \
+#    loglevel=7 \
+#    console=ttyAMA0
+
+
+
 $(call inherit-product, device/google/trout/aosp_trout_arm64.mk)
 
 include device/epam/aosp-xenvm-trout/aosp_xenvm_trout_common.mk
