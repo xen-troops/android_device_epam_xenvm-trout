14313f3cb5bff683c2072120a91563bc206825ea
Vladyslav Goncharuk <vladyslav_goncharuk@epam.com>
2023-05-29 22:07:24 +0300

    Added overriden impl. of the audio.primary.caremu
    There is a crash in Android 12 version of the "audio.primary.caremu"
    service implementation.
    
    This patch introduces the overriden implementation of this service,
    which fixes the crash.
    
    The fix of the crash is located in the 'audio_hw.c' line 1221.
    pthread_create call should be moved to a certain place in order to avoid
    a race condition.
    
    Initially the call was located here:
    
    https://cs.android.com/android/platform/superproject/+/android-
    12.1.0_r8:device/generic/car/emulator/audio/driver/audio_hw.c;l=1196
    
    Signed-off-by: Vladyslav Goncharuk <vladyslav_goncharuk@epam.com>
    Reviewed-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>

--- aosp_xenvm_trout_common.mk
+++ aosp_xenvm_trout_common.mk
@@ -5,4 +5,10 @@ PRODUCT_COPY_FILES += \
     device/epam/aosp-xenvm-trout/shared/config/init.recovery.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.xenvm_trout.rc \
     device/epam/aosp-xenvm-trout/shared/config/ueventd.rc:$(TARGET_COPY_OUT_RECOVERY)/root/ueventd.xenvm_trout.rc
 
-PRODUCT_PACKAGES += android.hardware.automotive.vehicle@2.0-virtualization-grpc-server-extended
+LOCAL_AUDIO_PROPERTIES ?= \
+    ro.hardware.audio.primary=caremu-extended \
+    ro.vendor.caremu.audiohal.out_period_ms=40 \
+    ro.vendor.caremu.audiohal.in_period_ms=40 \
+
+PRODUCT_PACKAGES += audio.primary.caremu-extended \
+                    android.hardware.automotive.vehicle@2.0-virtualization-grpc-server-extended
