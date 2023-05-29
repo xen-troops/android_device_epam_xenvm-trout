70b225483ccf8a4766a73bb39248233e9f95051c
Vladyslav Goncharuk <vladyslav_goncharuk@epam.com>
2023-05-29 21:59:14 +0300

    Override impl. of the virtualization-grpc-server
    In order to have 'trout' running we need to have virtualization GRPC
    server available. Vehicle hal relies on its presence. In the future, it
    should be available in DomD. But as far as we are experimenting with
    QEMU, we will deploy slightly extended ( gets more parameters from rc
    file ) version of it right inside Android.
    
    The following things are done:
    
    - Added minimalistic override of the virtualization-grpc-server (
    android.hardware.automotive.vehicle@2.0-virtualization-grpc-server-extended
    ) with modified list of parameters and changed service name
    - Added related sepolicy rules
    - Added service to the PRODUCT_PACKAGES
    
    Signed-off-by: Vladyslav Goncharuk <vladyslav_goncharuk@epam.com>
    Reviewed-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>

--- aosp_xenvm_trout_common.mk
+++ aosp_xenvm_trout_common.mk
@@ -1,4 +1,8 @@
+BOARD_SEPOLICY_DIRS += device/epam/aosp-xenvm-trout/sepolicy
+
 PRODUCT_COPY_FILES += \
     device/epam/aosp-xenvm-trout/shared/config/init.vendor.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.xenvm_trout.rc \
     device/epam/aosp-xenvm-trout/shared/config/init.recovery.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.xenvm_trout.rc \
     device/epam/aosp-xenvm-trout/shared/config/ueventd.rc:$(TARGET_COPY_OUT_RECOVERY)/root/ueventd.xenvm_trout.rc
+
+PRODUCT_PACKAGES += android.hardware.automotive.vehicle@2.0-virtualization-grpc-server-extended
