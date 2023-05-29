3bd4269b37c7681b10c0f1c910abd10de9bb3f39
Vladyslav Goncharuk <vladyslav_goncharuk@epam.com>
2023-05-29 21:47:02 +0300

    Introduction of the 'xenvm_trout' hardware
    It is a way to have a more deep definition of our own device.
    
    We want to use "androidboot.hardware=xenvm_trout" kernel flag. That
    would be a proper way to have our own config *.rc files being searched
    during the startup. One of them will avoid renaming of eth0 during
    startup of the system.
    
    - Introduced 'shared/config/ueventd.rc', which overrides
    'device/google/cuttlefish/shared/config/ueventd.rc'
    - Introduced 'shared/config/init.recovery.rc', which overrides
    'device/google/cuttlefish/shared/config/init.recovery.rc'
    - Extended 'aosp_xenvm_trout_common.mk' with copying 3 specific rc
    files to the vendor image with 'xenvm_trout' pattern in their names
    - Added 'vendor.ser.bt-uart' parameter to the
    'aosp_xenvm_trout_arm64.mk'. Usage of '/dev/hvc5' is declared in
    'ueventd.rc'. Without this declaration there were issues with startup.
    
    Signed-off-by: Vladyslav Goncharuk <vladyslav_goncharuk@epam.com>
    Reviewed-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>

--- aosp_xenvm_trout_common.mk
+++ aosp_xenvm_trout_common.mk
@@ -1,2 +1,4 @@
 PRODUCT_COPY_FILES += \
-    device/epam/aosp-xenvm-trout/shared/config/init.vendor.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.cutf_cvm.rc
+    device/epam/aosp-xenvm-trout/shared/config/init.vendor.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.xenvm_trout.rc \
+    device/epam/aosp-xenvm-trout/shared/config/init.recovery.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.xenvm_trout.rc \
+    device/epam/aosp-xenvm-trout/shared/config/ueventd.rc:$(TARGET_COPY_OUT_RECOVERY)/root/ueventd.xenvm_trout.rc
