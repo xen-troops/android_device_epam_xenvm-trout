8904e0d892a3efa032f66505941f0c0effb9846e
Andrii Chepurnyi <andrii_chepurnyi@epam.com>
2024-02-23 20:17:20 +0200

    Remove overriden_impl for Android 14
    Remove overriden_impl and *.rc files
    which was created for Android 12.
    
    Signed-off-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>
    Reviewed-by: Vladyslav Goncharuk <vladyslav_goncharuk@epam.com>

--- aosp_xenvm_trout_common.mk
+++ aosp_xenvm_trout_common.mk
@@ -1,17 +1,13 @@
 BOARD_SEPOLICY_DIRS += device/epam/aosp-xenvm-trout/sepolicy
 
-PRODUCT_COPY_FILES += \
-    device/epam/aosp-xenvm-trout/shared/config/init.vendor.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.xenvm_trout.rc \
-    device/epam/aosp-xenvm-trout/shared/config/init.recovery.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.xenvm_trout.rc \
-    device/epam/aosp-xenvm-trout/shared/config/ueventd.rc:$(TARGET_COPY_OUT_RECOVERY)/root/ueventd.xenvm_trout.rc
 
 LOCAL_AUDIO_PROPERTIES ?= \
-    ro.hardware.audio.primary=caremu-extended \
+    ro.hardware.audio.primary=caremu \
     ro.vendor.caremu.audiohal.out_period_ms=40 \
     ro.vendor.caremu.audiohal.in_period_ms=40 \
 
-LOCAL_VHAL_PRODUCT_PACKAGE ?= android.hardware.automotive.vehicle@2.0-virtualization-service-extended
+PRODUCT_PACKAGES += audio.primary.caremu
 
-PRODUCT_PACKAGES += audio.primary.caremu-extended
+
 
 PRODUCT_PACKAGE_OVERLAYS += device/epam/aosp-xenvm-trout/overlay
