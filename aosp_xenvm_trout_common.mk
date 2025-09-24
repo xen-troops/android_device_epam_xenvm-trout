653ca2c6e3bd34e44daa950cdda02a92af1d2b53
Andrii Chepurnyi <andrii_chepurnyi@epam.com>
2025-09-24 15:20:59 +0000

    Added overriden impl. of the audio.primary.caremu
    Current board doesn't have any mixers(audio as well), and original implementation
    crashes on mixer print. Fix crash just to be able to boot.
    
    Signed-off-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>
    Reviewed-by: Vladyslav Goncharuk <vladyslav_goncharuk@epam.com>
    Acked-by: Ruslan Shymkevych <ruslan_shymkevych@epam.com>

--- aosp_xenvm_trout_common.mk
+++ aosp_xenvm_trout_common.mk
@@ -5,7 +5,7 @@ BOARD_SEPOLICY_DIRS += device/epam/aosp-xenvm-trout/sepolicy
 PRODUCT_PACKAGES += dhcpclient.recovery
 
 LOCAL_AUDIO_PROPERTIES ?= \
-    ro.hardware.audio.primary=caremu \
+    ro.hardware.audio.primary=caremu-ext \
     ro.vendor.caremu.audiohal.out_period_ms=40 \
     ro.vendor.caremu.audiohal.in_period_ms=40 \
 
