25ba58ee4531995d154a90ef7ffbb1fd71c07ecb
Andrii Chepurnyi <andrii_chepurnyi@epam.com>
2025-09-30 16:34:29 +0200

    Revert "Use default vehicle HAL implementation"
    This reverts commit 31e3459a791ef8fd2250e6a39e89baf918a57a05.
    
    Signed-off-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>
    Acked-by: Ruslan Shymkevych <ruslan_shymkevych@epam.com>

--- aosp_xenvm_trout_arm64.mk
+++ aosp_xenvm_trout_arm64.mk
@@ -17,7 +17,7 @@ LOCAL_OEMLOCK_PRODUCT_PACKAGE := android.hardware.oemlock-service.example
 
 # To override VHAL, declare LOCAL_VHAL_PRODUCT_PACKAGE
 # prior to device/google/trout/aosp_trout_arm64.mk include
-LOCAL_VHAL_PRODUCT_PACKAGE = android.hardware.automotive.vehicle@2.0-default-service
+LOCAL_VHAL_PRODUCT_PACKAGE = android.hardware.automotive.vehicle@2.0-virtualization-service
 
 DEVICE_VIRTWIFI_PORT := eth0
 
