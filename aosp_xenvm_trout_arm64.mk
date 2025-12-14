36aeb62c4b8c853832d55f18123e780d62be94b5
Andrii Chepurnyi <andrii_chepurnyi@epam.com>
2025-12-14 22:06:18 +0000

    Switch graphics implementation to PVR instead of Mesa
    Use the PVR-based graphics implementation to support a shared GPU.
    
    Signed-off-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>
    Acked-by: Dmytro Firsov <dmytro_firsov@epam.com>

--- aosp_xenvm_trout_arm64.mk
+++ aosp_xenvm_trout_arm64.mk
@@ -23,6 +23,11 @@ PRODUCT_COPY_FILES += \
 
 LOCAL_OEMLOCK_PRODUCT_PACKAGE := android.hardware.oemlock-service.example
 
+
+PRODUCT_VENDOR_PROPERTIES += ro.hardware.egl=powervr
+PRODUCT_VENDOR_PROPERTIES += ro.hardware.vulkan=powervr
+
+
 # To override VHAL, declare LOCAL_VHAL_PRODUCT_PACKAGE
 # prior to device/google/trout/aosp_trout_arm64.mk include
 LOCAL_VHAL_PRODUCT_PACKAGE = android.hardware.automotive.vehicle@2.0-virtualization-service
@@ -49,6 +54,139 @@ PRODUCT_VENDOR_PROPERTIES += \
 PRODUCT_PACKAGES += \
     lisot
 
+# Composer 2.3
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.3-hal \
+    android.hardware.graphics.composer@2.3-passthrough \
+    android.hardware.graphics.composer@2.3-service \
+    hwcomposer.xenvm_trout_arm64 \
+
+# Img deps
+PRODUCT_PACKAGES += \
+    libdmabufinfo \
+    libprotobuf-cpp-lite \
+    perfetto_trace_protos \
+    libperfetto_client_experimental \
+    android.hardware.atrace@1.0.vendor \
+    android.hardware.dumpstate@1.0.vendor \
+    android.hardware.thermal@2.0.vendor \
+    android.hardware.thermal@1.0.vendor \
+    libion.vendor \
+    libdmabufheap.vendor \
+    libdumpstateutil.vendor \
+    android.hardware.memtrack-V1-ndk.vendor \
+    libdrm \
+    libarect \
+    perfetto_trace_protos
+
+# Global for IMG DDK
+
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.common@1.0-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.mapper@2.0-impl-2.1 \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.renderscript@1.0-impl \
+    libion \
+    libdrm \
+    libLLVM \
+    img-deps \
+
+
+
+# Graphics allocator/mapper HIDL HALs
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.mapper@2.0-impl-2.1
+
+# Graphics allocator AIDL V1 HAL
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator-V1-ndk.vendor
+
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0.vndk-sp \
+    android.hardware.graphics.mapper@2.0.vndk-sp \
+    android.hardware.graphics.mapper@2.1.vndk-sp \
+    android.hardware.graphics.common@1.0.vndk-sp \
+    android.hardware.atrace@1.0.vndk-sp \
+    libhwbinder.vndk-sp \
+    libbase.vndk-sp \
+    libcutils.vndk-sp \
+    libhardware.vndk-sp \
+    libhidlbase.vndk-sp \
+    libhidltransport.vndk-sp \
+    libutils.vndk-sp \
+    libc++.vndk-sp \
+    libRS_internal.vndk-sp \
+    libRSDriver.vndk-sp \
+    libRSCpuRef.vndk-sp \
+    libbcinfo.vndk-sp \
+    libblas.vndk-sp \
+    libft2.vndk-sp \
+    libpng.vndk-sp \
+    libcompiler_rt.vndk-sp \
+    libbacktrace.vndk-sp \
+    libunwind.vndk-sp \
+    libunwindstack.vndk-sp \
+    liblzma.vndk-sp \
+    libion.vndk-sp \
+    android.hardware.graphics.composer@2.1 \
+    android.hardware.graphics.allocator-V2-ndk.vendor \
+    libgralloctypes.vendor
+
+
+PRODUCT_PACKAGES += \
+    img_vintf_android.hardware.graphics.allocator.aidl-service.img-v2.xml \
+    img_vintf_android.hardware.memtrack.aidl.img.xml \
+    img_vintf_mapper.powervr.xml \
+    img_vintf_android.hardware.dumpstate@1.1-service.img.xml \
+    android.hardware.dumpstate@1.1-service.img.rc \
+    android.hardware.graphics.allocator.aidl-service.img.rc \
+    android.hardware.memtrack.aidl.img.rc \
+    hwperfbin2jsont \
+    pvrdebug \
+    pvrhtb2txt \
+    pvrhtbd \
+    pvrhwperf \
+    pvrhwperfd \
+    pvrlogdump \
+    pvrlogsplit \
+    pvrsrvctl \
+    pvrtld \
+    android.hardware.dumpstate@1.1-service.img \
+    android.hardware.graphics.allocator-service \
+    android.hardware.memtrack-service.img \
+    rgx.fw.35.2.1632.35 \
+    rgx.sh.35.2.1632.35 \
+    libgpudataproducer \
+    libIMGegl \
+    libpvrANDROID_WSEGL \
+    libPVROCL \
+    libPVRScopeServices \
+    libsrv_um \
+    libufwriter \
+    libusc \
+    libEGL_powervr \
+    libGLESv1_CM_powervr \
+    libGLESv2_powervr \
+    gralloc.xenvm_trout_arm64 \
+    mapper.powervr \
+    vulkan.powervr
+
+
+# Graphics composer HIDL HAL (service added below)
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.1.vendor \
+    android.hardware.graphics.composer@2.1-impl
+
+# Dumpstate
+PRODUCT_PACKAGES += \
+    android.hardware.dumpstate@1.1 \
+    android.hardware.dumpstate@1.1.vendor
+
+LOCAL_DEVICE_FCM_MANIFEST_FILE = device/epam/aosp-xenvm-trout/manifest.xml
+
+$(call inherit-product, device/epam/aosp-xenvm-trout/build/graphics.mk)
 $(call inherit-product, device/google/cuttlefish/shared/virgl/device_vendor.mk)
 $(call inherit-product, device/google/trout/aosp_trout_arm64.mk)
 $(call inherit-product, device/epam/aosp-xenvm-trout/aosp_xenvm_trout_common.mk)
---
+++ build/kernel_modules.mk
@@ -0,0 +1,50 @@
+#
+# Copyright (C) 2019 GlobalLogic
+# Copyright (C) 2021 EPAM systems
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+ifeq ($(KERNEL_MODULES_OUT),)
+$(error "KERNEL_MODULES_OUT is not set")
+endif
+
+ifeq ($(PRODUCT_OUT),)
+$(error "PRODUCT_OUT is not set")
+endif
+
+BOARD_VENDOR_KERNEL_MODULES += \
+	$(KERNEL_MODULES_OUT)/rcar-rvgc-drm.ko \
+	$(KERNEL_MODULES_OUT)/rcar-vivid.ko \
+
+WLAN_KM_SRC             := hardware/realtek/rtl8812au_km
+WLAN_KM_OUT             := $(PRODUCT_OUT)/obj/WLAN_KM_OBJ
+WLAN_KM_OUT_ABS         := $(abspath $(WLAN_KM_OUT))
+WLAN_KM                 := $(WLAN_KM_OUT)/8812au.ko
+
+$(WLAN_KM):
+	mkdir -p $(WLAN_KM_OUT_ABS)
+	cp -pR $(WLAN_KM_SRC)/* $(WLAN_KM_OUT_ABS)/
+	PATH=$(ANDROID_MAKE_PATH):$$PATH $(ANDROID_MAKE) -C $(WLAN_KM_OUT_ABS) $(KERNEL_CFLAGS) \
+		KERNELDIR=$(KERNEL_OUT_ABS) \
+		WORKDIR=$(WLAN_KM_OUT_ABS) rcar_defconfig
+	PATH=$(ANDROID_MAKE_PATH):$$PATH $(ANDROID_MAKE) -C $(WLAN_KM_OUT_ABS) $(KERNEL_CFLAGS) \
+		KERNELDIR=$(KERNEL_OUT_ABS) WORKDIR=$(WLAN_KM_OUT_ABS) \
+		M=$(WLAN_KM_OUT_ABS) modules
+	cp $(WLAN_KM) $(KERNEL_MODULES_OUT)/
+
+KERNEL_EXT_MODULES += \
+	$(WLAN_KM)
+
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES += \
+	$(BOARD_RECOVERY_KERNEL_MODULES)
