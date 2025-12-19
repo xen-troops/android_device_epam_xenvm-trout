3e8f75e5e9013541fade7df03e0e1487a8a7819c
Andrii Chepurnyi <andrii_chepurnyi@epam.com>
2025-12-19 21:45:53 +0000

    Integrate correct disfwk_fe enablement
    Use the appropriate firmware before inserting disfwk_fe.ko.
    
    Signed-off-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>
    Acked-by: Dmytro Firsov <dmytro_firsov@epam.com>

---
+++ init/displ_fe.sh
@@ -0,0 +1,8 @@
+#!/system/bin/sh
+# Remote proc part, need to start communication with real-time cluster
+cd /vendor/firmware/
+echo ./disfwk.elf > /sys/class/remoteproc/remoteproc0/firmware
+echo start > /sys/class/remoteproc/remoteproc0/state
+# WA: need to be sure that Taurus initialization is complete before starting KM
+sleep 2
+insmod /vendor/lib/modules/disfwk_fe.ko
